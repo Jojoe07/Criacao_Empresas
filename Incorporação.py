fe598ec59babfb6a1a39f04417950a099048469e
Jojoe07 <109483495+Jojoe07@users.noreply.github.com>
2025-02-11 22:37:32 -0300

    Update Incorporação.py

--- Incorporação.py
+++ Incorporação.py
@@ -3,9 +3,9 @@ import streamlit as st
 
 st. title("Hacka Itaú Empresas")
 st.subheader("Extrato")
-col1 , col2 = st.columns(2 , gap = "medium")
-col1.write("Jornada atual")
-col2.write = embed_code = """<iframe style="border: 1px solid rgba(0, 0, 0, 0.1);" width="200" height="450" src="https://embed.figma.com/proto/OYgx6p20H1Y0dnoGkCmESh/Hacka-Empresas?node-id=1-15&scaling=scale-down&content-scaling=fixed&page-id=0%3A1&starting-point-node-id=1%3A16&embed-host=share" allowfullscreen></iframe>"""
+tab1 , tab2 = st.tabs(["Jornada atual", "Visão atual"])
+tab1.write("Jornada atual")
+tab2.write = embed_code = """<iframe style="border: 1px solid rgba(0, 0, 0, 0.1);" width="200" height="450" src="https://embed.figma.com/proto/OYgx6p20H1Y0dnoGkCmESh/Hacka-Empresas?node-id=1-15&scaling=scale-down&content-scaling=fixed&page-id=0%3A1&starting-point-node-id=1%3A16&embed-host=share" allowfullscreen></iframe>"""
 
 st.components.v1.html(embed_code,height=500)
 
