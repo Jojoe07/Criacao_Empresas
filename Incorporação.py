a1dc6471eec7495923dea0781df157bdc09d3d4a
Jojoe07 <109483495+Jojoe07@users.noreply.github.com>
2025-02-11 22:20:46 -0300

    Update Incorporação.py

--- Incorporação.py
+++ Incorporação.py
@@ -8,7 +8,7 @@ st.subheader("Extrato")
 
 embed_code = """<iframe style="border: 1px solid rgba(0, 0, 0, 0.1);" width="200" height="450" src="https://embed.figma.com/proto/OYgx6p20H1Y0dnoGkCmESh/Hacka-Empresas?node-id=1-15&scaling=scale-down&content-scaling=fixed&page-id=0%3A1&starting-point-node-id=1%3A16&embed-host=share" allowfullscreen></iframe>"""
 
-st.components.v1.html(embed_code,height=480)
+st.components.v1.html(embed_code,height=500)
 
 
 
